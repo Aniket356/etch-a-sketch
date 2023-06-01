9efc3e57f721a13704b2f9125bba30e59f204d08
Aniket <t.aniket561@gmail.com>
2023-06-01 19:48:09 +0530

    add random color mode

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//if random color mode is on, the boxes will change color to a random color when mosue is hovered
+let randomColorMode = false;
+const randomColorBtn = document.querySelector('#random_color');
+randomColorBtn.addEventListener('click', function () {
+    randomColorMode = randomColorMode ? false : true;
+})
+
 //this will generate a new grid and remove the previous one
 function create_new_grid(n){
     const container = document.querySelector('.grid-container');
@@ -21,7 +28,16 @@ function create_new_grid(n){
     const gridBoxes = document.querySelectorAll('.row>div');
     gridBoxes.forEach(box => {
         box.addEventListener('mouseover', function() {
-            this.classList.add('hovered');
+            if(randomColorMode){
+                //gnereate a random rgb value if random color mode is on
+                const red = Math.floor(Math.random() * (256));
+                const green = Math.floor(Math.random() * (256));
+                const blue = Math.floor(Math.random() * (256));
+                this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            }
+            else{
+                this.style.backgroundColor = 'black';
+            }
         })
     })
 }
@@ -32,12 +48,11 @@ create_new_grid(16);
 //button that can generate a new grid
 const grid_btn = document.querySelector('#grid_generator');
 grid_btn.addEventListener('click', function() {
-    const n = prompt("Enter the number of rows and columns");
-    if(n > 99 || n < 1){
-        alert("Value should be more than 0 and less than 100");
+    const n = parseInt(prompt("Enter the number of rows and columns"));
+    if(n > 0 && n < 101){
+        create_new_grid(n);
     }
     else{
-        console.log(n);
-        create_new_grid(n);
+        alert("Please enter a valid number between 1 and 100");
     }
 })
